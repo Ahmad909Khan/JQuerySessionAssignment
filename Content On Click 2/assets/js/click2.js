b2fe8a578fbc9ed8a5e74b86c79bf7ca73203dd9
Ahmad909Khan <you@example.com>
2022-01-18 10:50:17 +0530

    added 2nd rendition of JQuery button assignment.

---
+++ Content On Click 2/assets/js/click2.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#btn1").click(function () {
+        $("#content1").toggle()
+    })
+    $("#btn2").click(function () {
+        $("#content2").toggle()
+    })
+    $("#btn3").click(function () {
+        $("#content3").toggle()
+    })
+})
